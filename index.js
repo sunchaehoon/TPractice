e827ea3b92c05e35070dbd0f25cbab240eb88e39
sunchaehoon <sunchaehoon@naver.com>
2022-06-23 20:16:44 +0900

    6/23tsp

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ var project = {
     days: 30,
     started: true,
 };
+var 회원 = 123; // Union Type(타입 2개이상 합친 새로운 타입만들기)
+var 회원들 = [1, '2', 3];
+var 이름2; //(any랑 비슷함)모든 자료형 허용
+// 변수가 숫자타입이어야 오류 없이 숫자 계산을 해줌
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: 'Phil',
+    friend: 'John'
+};
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher];
+function fcn(x) {
+    return x * 2;
+}
+function 함수2(x) {
+} //파라미터가 옵션일 경우에 파라미터변수에 '?' 붙임. 타입 지정 안하면 undefined와 같음
--- index.ts
+++ index.ts
@@ -19,3 +19,31 @@ let project : {
     days : 30,
     started : true,
 }
+
+let 회원 :number | string = 123;    // Union Type(타입 2개이상 합친 새로운 타입만들기)
+
+let 회원들 :(number | string) [] = [1, '2', 3];
+
+let 이름2 :unknown;     //(any랑 비슷함)모든 자료형 허용
+// 변수가 숫자타입이어야 오류 없이 숫자 계산을 해줌
+
+let 학교 : {
+    score :(number | boolean)[],
+    teacher :string,
+    friend :string | string[],
+} = {
+    score : [100, 97, 84],
+    teacher : 'Phil',
+    friend : 'John'
+}
+학교.score[4] = false;
+학교.friend = ['Lee' , 학교.teacher]
+
+
+function fcn(x :number) :number {   //리턴값의 타입을 지정    
+    return x*2;
+}
+
+function 함수2(x? :number) {
+
+}   //파라미터가 옵션일 경우에 파라미터변수에 '?' 붙임. 타입 지정 안하면 undefined와 같음
